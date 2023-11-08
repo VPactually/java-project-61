40a77dfefafda659bb4a78fe104286148a133157
Vladislav Pomozov <vpactually@gmail.com>
2023-11-08 11:30:34 +0300

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+    application
+}
+
+application {
+    mainClass.set("src/main/java/hexlet/code/App.java")
+}
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
