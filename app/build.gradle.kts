5a96249c158a7210ac51e8e70469e3475f417df5
Vladislav Pomozov <vpactually@gmail.com>
2023-11-08 11:59:17 +0300

    changed Hello World -> Welcome to the Brain Games!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,21 +3,31 @@ plugins {
     application
 }
 
+tasks.withType<JavaCompile> {
+    options.release = 20
+}
+
 application {
     mainClass.set("src/main/java/hexlet/code/App.java")
+
 }
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
+
 repositories {
     mavenCentral()
+
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
     useJUnitPlatform()
+
 }
+
