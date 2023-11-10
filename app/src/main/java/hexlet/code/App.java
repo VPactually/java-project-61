0acacbac28fe4ff0c0f0f9387d374da1a059c7bc
Vladislav Pomozov <vpactually@gmail.com>
2023-11-10 04:25:05 +0300

    small fixes

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,7 +3,6 @@ package hexlet.code;
 public class App {
     public static void main(String[] args) {
         int choice = 0;
-        String username;
         System.out.print("""
                 Please enter the game number and press Enter.
                 1 - Greet
@@ -11,10 +10,8 @@ public class App {
                 0 - Exit
                 Your choice:\s""");
         switch (Scanner.getInt()) {
-            case 1 -> Cli.greeting();
             case 2 -> {
-                username = Cli.greeting();
-                System.out.println(Games.gameEven(username));
+                System.out.println(Games.gameEven());
             }
             default -> {
             }
--- app/src/main/java/hexlet/code/Games.java
+++ app/src/main/java/hexlet/code/Games.java
@@ -1,18 +1,29 @@
 package hexlet.code;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Games {
-    public static String gameEven(String username) {
+    public static String gameEven() {
         Random random = new Random();
         String answer = "";
         String correctAnswer = "";
+        String username = "";
         int numbers = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nWelcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        if (scanner.hasNextLine()) {
+            username = scanner.nextLine();
+            System.out.println("Hello, " + username + "!");
+        } else {
+            return "Invalid input.";
+        }
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         for (int i = 0; i < 3; ) {
             numbers = random.nextInt(0, 100);
             System.out.println("Question: " + numbers + "\nYour answer: ");
-            answer = Scanner.getString();
+            answer = hexlet.code.Scanner.getString();
             correctAnswer = numbers % 2 == 0 ? "yes" : "no";
             if (answer.equals(correctAnswer)) {
                 System.out.println("Correct!");
