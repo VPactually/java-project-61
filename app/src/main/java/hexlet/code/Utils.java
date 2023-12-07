6dcf2c0518edf21f881a05116f3f4a03412c02bf
Vladislav Pomozov <vpactually@gmail.com>
2023-12-07 10:11:51 +0300

    checkstyle fixes

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -4,11 +4,12 @@ import java.util.Random;
 
 public class Utils {
 
-    public static int getRandomNumber (int lower_bound, int upper_bound) {
+    public static int getRandomNumber(int lowerBound, int upperBound) {
         Random random = new Random();
-        return random.nextInt(lower_bound,upper_bound);
+        return random.nextInt(lowerBound, upperBound);
     }
-    public static int getRandomNumber (int upper_bound) {
-        return getRandomNumber(0,upper_bound);
+
+    public static int getRandomNumber(int upperBound) {
+        return getRandomNumber(0, upperBound);
     }
 }
