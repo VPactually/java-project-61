7269738fb7b5ce99f153ca738ffe7a00393a9786
Vladislav Pomozov <vpactually@gmail.com>
2023-12-10 20:43:16 +0300

    checkstyle fixes

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -13,7 +13,7 @@ public class Prime {
         if (n == 2) {
             return true;
         }
-        for (int i = 3; i <= Math.sqrt(n); i+=2) {
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
             if (n % i == 0) {
                 return false;
             }
